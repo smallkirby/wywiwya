53b9540015251a59d9a5c29112bd02c18a291a17
smallkirby <ssmallkirby@gmail.com>
2022-03-25 12:38:28 +0900

    ✨ impl user badge

---
+++ components/MeBadge.vue
@@ -0,0 +1,39 @@
+<template>
+  <layout-wrapper>
+    <div class="bg-white rounded-xl shadow-xl w-56  h-12 mt-4 py-2 px-3">
+      <button v-if="me !== null" class="flex items-center w-full">
+        <img :src="me.photoURL" class="w-8 rounded-full mr-4">
+        <p> {{ me.displayName }} </p>
+      </button>
+      <button v-else class="w-full pt-1">
+        not logged in
+      </button>
+    </div>
+  </layout-wrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+import { Login } from '@/lib/auth';
+
+export default Vue.extend({
+  name: 'MeBadge',
+  data () {
+    return {};
+  },
+
+  computed: {
+    ...mapGetters([
+      'me',
+    ]),
+  },
+
+  methods: {
+    async doLogin () {
+      await Login();
+      this.$router.push('/');
+    },
+  },
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="bg-skwhite w-full h-full">
-    <wywiwya-logo />
+    <div class="flex justify-between items-end">
+      <wywiwya-logo />
+      <div class="mr-8">
+        <me-badge />
+      </div>
+    </div>
     <div class="w-7/12 h-screen mx-auto">
       <layout-header />
       <div class="my-4">
