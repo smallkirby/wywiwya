a1c014f111a97d76d4512f59729b5563be12ea1d
smallkirby <ssmallkirby@gmail.com>
2022-03-27 13:16:16 +0900

    🐛✏️ push typing file

---
+++ typings/diary.d.ts
@@ -0,0 +1,13 @@
+import { UID } from '~/store/state';
+
+export type DateID = string;
+
+export type Diary = {
+  dateID: DateID,
+  createdAt: Date,
+  lastUpdatedAt: Date,
+  isPublic: boolean,
+  isTemporary: boolean,
+  contentMd: string,
+  author: UID,
+}
