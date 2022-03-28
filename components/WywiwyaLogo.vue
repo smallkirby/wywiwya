3bc10b7be466c44e9ab2ec89937c00b3b1135fca
smallkirby <ssmallkirby@gmail.com>
2022-03-29 01:34:53 +0900

    💄 change color theme of preview box

--- components/WywiwyaLogo.vue
+++ components/WywiwyaLogo.vue
@@ -2,12 +2,12 @@
   <layout-wrapper>
     <button>
       <nuxt-link to="/">
-        <div class="flex ml-4 mr-2 items-center hover:bg-skwhite-dark rounded-md">
-          <div class="text-6xl mr-2">
+        <div class="flex ml-4 mr-2 items-center rounded-md">
+          <div class="text-6xl mr-2 text-[#8EC07C]">
             WYWIWYA
           </div>
           <img src="~/static/logo/wywiwya.png" class="w-10 md:hidden">
-          <div class="flex-col hidden md:block">
+          <div class="flex-col hidden md:block text-[#427B58] mt-2">
             <div class="text-xs text-center leading-none">
               WHAT YOU WRITE
             </div>
--- components/editor/PreviewBox.vue
+++ components/editor/PreviewBox.vue
@@ -6,7 +6,7 @@
     <div
       id="previewBox"
       ref="previewBox"
-      class="h-full w-full px-8 ml-4 mr-4 overflow-y-scroll"
+      class="h-full w-full pl-2 pr-8 ml-4 mr-4 overflow-y-scroll"
     />
   </div>
 </template>
@@ -50,4 +50,35 @@ export default Vue.extend({
   height: calc(100vh - 195px) !important;
   padding-bottom: 30px;
 }
+
+#previewBox h1,h2,h3,h4,h5 {
+  margin-top: 0.8rem;
+  margin-bottom: 1.2rem;
+  padding-bottom: 0.8rem;
+  padding-right: 2.0rem;
+  border-bottom: solid 1px;
+  border-color: rgba(23, 33, 49, var(--tw-border-opacity));
+}
+
+#previewBox p {
+  padding-left: 1rem;
+}
+
+#previewBox li {
+  list-style: square;
+  margin-bottom: 0.2rem;
+}
+
+#previewBox strong {
+  color: #B16286;
+}
+
+#previewBox code {
+  color: #6272A4;
+}
+
+#previewBox a {
+  color: #076678;
+}
+
 </style>
